9082c4b82637724ac993c65bdea7a08ccc8d2bc2
KashishSingh1213 <singhkashish364@gmail.com>
2025-06-30 15:35:57 +0530

    do user input

--- Day1/userinput_output.py
+++ Day1/userinput_output.py
@@ -1,10 +1,8 @@
-# x = input("enter your name: ")
-# print("hello", x)
+x = input("enter your name: ")
+print("hello", x)
 
-# This code takes user input and prints a greeting message.
-# x= input("enter your age")
-# print("age",+x)
+This code takes user input and prints a greeting message.
+x= input("enter your age")
+print("age",+x)
 
-a, b = input("Enter two names: ").split()
-print("First name:", a)
-print("Second name:", b)
+
