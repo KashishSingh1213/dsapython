71dcfeb8e0144898064dda8b4455fb51f4bfa9da
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-01 11:47:02 +0530

    Python program for the above approach

--- Day1/userinput_output.py
+++ Day1/userinput_output.py
@@ -1,7 +1,7 @@
 x = input("enter your name: ")
 print("hello", x)
 
-This code takes user input and prints a greeting message.
+# This code takes user input and prints a greeting message.
 x= input("enter your age")
 print("age",+x)
 
