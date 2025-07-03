523ec226a6aaba8c2fb14ef859a24d00712a7638
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-03 10:32:29 +0530

    solve the question

--- Day3/palindrome.py
+++ Day3/palindrome.py
@@ -1,16 +1,16 @@
 #check the palindrome
 # lets take a example of n=1234 and i want to check the this is palindrome or not 
 
-# n = 1234
-# num = n
-# result = 0
+n = 1234
+num = n
+result = 0
 
-# while num > 0:
-#     ld = num % 10                 # get last digit
-#     result = (result * 10) + ld   # build reversed number
-#     num = num // 10               # remove last digit
+while num > 0:
+    ld = num % 10                 # get last digit
+    result = (result * 10) + ld   # build reversed number
+    num = num // 10               # remove last digit
 
-# print("The n value is:", result)
+print("The n value is:", result)
 
 
 
@@ -23,3 +23,14 @@ while num>0:
     result=(result*10)+ld
     num = num//10
     print("the value of n is:",result)
+    
+    
+# the question is 121
+n=121
+num=n
+result=0
+while num>0:
+    ld=num%10
+    result=(result*10)+ld
+    num = num//10
+    print('the value of n is:',result)
