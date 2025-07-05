87b533dadcba52ad68592fe4b8f29f7574503cad
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-05 10:04:51 +0530

    solve the question of freqmap

---
+++ day4/freqmap.py
@@ -0,0 +1,25 @@
+#method 1 to store the no.
+nums = [1, 2, 4, 5, 3, 5, 7, 7, 1, 2, 3, 5]
+freqmap = dict()
+
+for i in range(len(nums)):
+    if nums[i] in freqmap:
+        freqmap[nums[i]] += 1
+    else:
+        freqmap[nums[i]] = 1
+
+print("Frequency Map:", freqmap)
+
+
+#method 2 to store the no.
+# Using a dictionary as a HashMap to store frequency
+nums = [1, 2, 4, 5, 3, 5, 7, 7, 1, 2, 3, 5]
+hashmap = {}  # This is your HashMap
+
+for num in nums:
+    if num in hashmap:
+        hashmap[num] += 1
+    else:
+        hashmap[num] = 1
+
+print("Frequency using HashMap:",hashmap)
