a8fc8db739625c90aab01dfcb64829816d42d5f4
Kashish Singh <youremail@example.com>
2025-07-08 20:43:04 +0530

    solve question

--- day5/parameterized_functional_recursion.py
+++ day5/parameterized_functional_recursion.py
@@ -16,3 +16,13 @@ def get_numbers(i, n):
 
 result = get_numbers(1, 10)
 print("Numbers from 1 to 10:", result)
+
+
+# Recursive function to calculate product of 1 to n
+def product_rec(prod, i, n):
+    if i > n:
+        print("Product:", prod)
+        return
+    product_rec(prod * i, i + 1, n)
+
+product_rec(1, 1, 5)  # Product of 1 to 5 = 120
