808c8b2ba981ad2ddc088350ba5f8addad6b2ca3
Kashish Singh <youremail@example.com>
2025-07-08 16:35:58 +0530

    code of factorial

--- day6/factorial.py
+++ day6/factorial.py
@@ -7,9 +7,20 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
+def factorial_iterative(n):
+    # Base case
+    if n == 0 or n == 1:
+        return 1
+    # Iterative solution
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
 # Example usage
 num = int(input("Enter a number: "))
 if num < 0:
     print("Factorial is not defined for negative numbers.")
 else:
-    print("Factorial of", num, "is", factorial(num))
+    print("Recursive Factorial of", num, "is", factorial(num))
+    print("Iterative Factorial of", num, "is", factorial_iterative(num))
