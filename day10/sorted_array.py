7a5c77578228b7bf461b25434efdd694c10e36d5
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-11 20:49:42 +0530

    solve the question check the array is sorted or not

--- day10/sorted_array.py
+++ day10/sorted_array.py
@@ -6,3 +6,12 @@ def is_sorted(arr):
             return False
     return True
 print(is_sorted(num))  # Output: True
+
+#check the array is not sorted
+num2=[1,2,3,4,5,6,7,8,9,0]
+def is_sorted(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            return False
+    return True
+print(is_sorted(num2))  # Output: False
