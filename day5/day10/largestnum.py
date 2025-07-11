8623ab74452a33de3806f4fb56936a90e74c9acd
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-11 20:38:37 +0530

    find the second largest number

--- day5/day10/largestnum.py
+++ day5/day10/largestnum.py
@@ -1,10 +1,24 @@
-#find the largest numberin array
-nums=[55,32,97,99,3,67]
-def find_largest(nums):
-    largest = nums[0]
+# #find the largest numberin array
+# nums=[55,32,97,99,3,67]
+# def find_largest(nums):
+#     largest = nums[0]
+#     for num in nums:
+#         if num > largest:
+#             largest = num
+#     return largest
+
+# print(find_largest(nums))
+
+
+#find the second largest number in array
+nums=[55,34,87,22,12]
+def find_second_largest(nums):
+    largest = second_largest = float('-inf')
     for num in nums:
         if num > largest:
+            second_largest = largest
             largest = num
-    return largest
-
-print(find_largest(nums))
+        elif num > second_largest and num != largest:
+            second_largest = num
+    return second_largest
+print(find_second_largest(nums))
