17d9a84457d6b45fd885943543879a49e7b8fb12
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-12 20:24:31 +0530

    solve question

--- day11/remove_duplicate_array.py
+++ day11/remove_duplicate_array.py
@@ -64,3 +64,15 @@ print(right_rotate_k([1, 2, 3, 4, 5], 2))  # Output: [4, 5, 1, 2, 3]
 # Example outputs:
 # The array [1, 2, 3, 4, 5] becomes
 # [4, 5, 1, 2, 3] after the right rotation.
+
+#moves all zeroes to the end of the array
+def move_zeroes_to_end(arr):
+    non_zero_index = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[non_zero_index] = arr[i]
+            non_zero_index += 1
+    for i in range(non_zero_index, len(arr)):
+        arr[i] = 0
+    return arr
+print(move_zeroes_to_end([0, 1, 0, 3, 12]))  # Output: [1, 3, 12, 0, 0]
