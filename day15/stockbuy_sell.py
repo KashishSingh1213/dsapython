c5da90abcef90b21a860e7fd4cace52319201429
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-16 11:15:32 +0530

    solve using otimal solution

--- day15/stockbuy_sell.py
+++ day15/stockbuy_sell.py
@@ -21,3 +21,14 @@ for i in range(0,n):
             max_profit=max(max_profit,profit)
         else:
             break  # Stop if the price does not increase
+
+
+#optimal solution
+prices=[7,2,1,5,6,4,8]
+n=len(prices)
+max_profit=0
+for i in range(1,n):
+    if prices[i]>prices[i-1]:
+        profit=prices[i]-prices[i-1]
+        max_profit+=profit
+print("Maximum Profit (Optimal):", max_profit)
