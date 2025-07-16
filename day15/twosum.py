0d13866d6be6537a954e695ff932e3b672abab1a
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-16 10:33:22 +0530

    solve teo sum using otimal solution

--- day15/twosum.py
+++ day15/twosum.py
@@ -1,4 +1,5 @@
 #two sum problem in python
+#using brute force approach
 nums=[5,9,1,2,4,15,6,3]
 target=13
 n=len(nums)
@@ -7,7 +8,18 @@ for i in range(0,n-1):
         if nums[i]+nums[j]==target:
             print("Indices:", i, j)
             print("Numbers:", nums[i], nums[j])
-            
-        
 
+        
+#two sum problem in python using optimal soutions
+nums=[5,9,1,2,4,15,6,3]
+n=len(nums)
+target=13
+hash_map={}
+for i in range(0,n):
+    remaining=target-nums[i]
+    if remaining in hash_map:
+        print("Indices:", hash_map[remaining], i)
+        print("Numbers:", nums[hash_map[remaining]], nums[i])
+    
+# The output of each function is printed
             
