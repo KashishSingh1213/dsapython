b9737e097c5eeab4c07458ee73e8ae9461378ae6
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-16 11:00:01 +0530

    solve otimal soution

--- day15/maximum_subarray.py
+++ day15/maximum_subarray.py
@@ -12,3 +12,15 @@ for i in range(n):
         maxi = max(maxi, total)
 
 print("Maximum Subarray Sum (Brute Force):", maxi)
+
+#solve in otimal solution
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+n=len(nums)
+maxi = float('-inf')  # Initialize with the lowest possible number
+total = 0
+for i in range(n):
+    total += nums[i]
+    maxi = max(maxi, total)
+    if total < 0:
+        total = 0
+print("Maximum Subarray Sum (Optimal):", maxi)
