79a1ff5affae347ef5fed4f6e6a19535585aa39b
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-16 11:13:59 +0530

    only cell

--- day15/stockbuy_sell.py
+++ day15/stockbuy_sell.py
@@ -8,3 +8,16 @@ for i in range(n):
             profit=prices[j]-prices[i]
             max_profit=max(max_profit,profit)
 print("Maximum Profit:", max_profit)
+
+
+#if i can only sell the price
+prices=[7,2,1,5,6,4,8]
+n=len(prices)
+max_profit=1
+for i in range(0,n):
+    for j in range(i+1,n):
+        if prices[j]>prices[i]:
+            profit=prices[j]-prices[i]
+            max_profit=max(max_profit,profit)
+        else:
+            break  # Stop if the price does not increase
