a9eb38d2f89f26dda2a66fc60a024440cc8b1503
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-19 22:14:01 +0530

    .

--- day11/remove_duplicate_array.py
+++ day11/remove_duplicate_array.py
@@ -79,3 +79,5 @@ print(move_zeroes_to_end([0, 1, 0, 3, 12]))  # Output: [1, 3, 12, 0, 0]
 # Explanation:
 # The function move_zeroes_to_end iterates through the array and moves all non-zero elements
 # to the front, keeping track of the index for non-zero elements.
+
+# The remaining elements are set to zero.
