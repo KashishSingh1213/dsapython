2b947a40a1e600d0f77522ac341b95870da3310e
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-20 10:42:56 +0530

    solve the question

--- day19/binary_search.py
+++ day19/binary_search.py
@@ -49,3 +49,21 @@ else:
 # The binary_search function takes a sorted array and a target value as input.
 # It uses a while loop to repeatedly divide the search interval in half.
 # If the target value is found, it returns the index; otherwise, it returns -1
+
+#iterative
+nums=[2,4,6,7,9,11,18,19]
+target=7
+def binary_search_iterative(nums,target):
+    n=len(nums)
+    low=0
+    high=n-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]==target:
+            return mid
+        elif nums[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+    
