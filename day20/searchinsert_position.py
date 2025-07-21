b4984b4973323be6c10e741164eb8eaad28ee489
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-21 20:53:33 +0530

    add content

--- day20/searchinsert_position.py
+++ day20/searchinsert_position.py
@@ -1,4 +1,6 @@
 #search insert position
+#all elemnts are arranged
+#array is sorted in ascending order
 nums=[1,3,4,5,8,9,14,15,19,20]
 def search_insert(nums,target):
     low=0
@@ -12,4 +14,3 @@ def search_insert(nums,target):
         else:
             high=mid-1
     return low
-    
