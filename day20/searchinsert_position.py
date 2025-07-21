846e3f8dfd5de655ec4e3cf7f9a460fc37db8e66
KashishSingh1213 <singhkashish364@gmail.com>
2025-07-21 20:50:05 +0530

    search insert position

---
+++ day20/searchinsert_position.py
@@ -0,0 +1,15 @@
+#search insert position
+nums=[1,3,4,5,8,9,14,15,19,20]
+def search_insert(nums,target):
+    low=0
+    high=len(nums)-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]==target:
+            return mid
+        elif nums[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+    return low
+    
