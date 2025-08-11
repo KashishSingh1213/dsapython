33444167df46eb90e601413616570e68a8bcead6
KashishSingh1213 <singhkashish364@gmail.com>
2025-08-11 20:05:33 +0530

    .

--- day25/ContainerWithMostWater.py
+++ day25/ContainerWithMostWater.py
@@ -50,3 +50,4 @@ if __name__ == "__main__":
     # Space complexity: O(1)
 # The two-pointer technique is used to find the maximum area of water that can be contained between the lines represented by the heights.
 # Selection sort is not applicable here as we are not sorting the array but finding the maximum area.
+# Selection sort is not applicable here as we are not sorting the array but finding the maximum area.
