453dbdbd77232f0d3a6d97d00932b8665f91e698
KashishSingh1213 <singhkashish364@gmail.com>
2025-08-15 10:26:34 +0530

    .

--- Day3/palindrome.py
+++ Day3/palindrome.py
@@ -146,3 +146,13 @@ else:
     print(f"{num} is not a palindrome.")
 
 # Function to check if a number is a palindrome
+def is_palindrome(n):
+    original = n
+    reverse = 0
+
+    while n > 0:
+        digit = n % 10
+        reverse = reverse * 10 + digit
+        n = n // 10
+
+    return original == reverse
