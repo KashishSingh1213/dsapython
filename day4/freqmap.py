7de9185daa444ea00ce876acb04bbf736a7efd02
KashishSingh1213 <singhkashish364@gmail.com>
2025-08-18 19:36:56 +0530

    .

--- day4/freqmap.py
+++ day4/freqmap.py
@@ -30,3 +30,4 @@ print("Frequency of 5:", hashmap.get(5, 0))  # Get frequency of 5, default to 0 
 # We iterate through each number in the list and update its count in the hashmap.
 # Finally, we print the entire frequency map and also demonstrate how to get the frequency of a specific number (5 in this case).
 #example of if-else statement
+num = 10
